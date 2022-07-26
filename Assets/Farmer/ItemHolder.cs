292781649a54501e88271acba452f30dcafdfafc
August Alexandersson <augustalexandersson@gmail.com>
2022-07-26 16:45:47 +0200

    Pickup mechanic

--- Assets/Farmer/ItemHolder.cs
+++ Assets/Farmer/ItemHolder.cs
@@ -21,7 +21,7 @@ public class ItemHolder : MonoBehaviour
         _item = item.gameObject;
 
         item.PickedUp();
-        
+
         DidHoldItem?.Invoke();
     }
 
@@ -36,7 +36,23 @@ public class ItemHolder : MonoBehaviour
     {
         if (HoldingItem())
         {
-            _item.transform.position = pivot.transform.position;
+            _item.transform.position = pivot.transform.position + pivot.transform.forward * .7f;
         }
+    }
+
+    public void Drop()
+    {
+        _item.GetComponent<Pickupable>().Dropped();
+
+        var pivotTransform = pivot.transform;
+        var newPosition = pivotTransform.position + pivotTransform.forward.normalized * 1f + Vector3.up;
+        var gridAlignedPosition = FarmGridUtils.GridAlign(newPosition);
+        Debug.Log("DROP POSITION: " + newPosition + ", GRIDPOS: " + gridAlignedPosition);
+        // _item.transform.position = gridAlignedPosition;
+        _item.GetComponent<Rigidbody>().MovePosition(gridAlignedPosition);
+
+        DidDropItem?.Invoke();
+
+        _item = null;
     }
 }
