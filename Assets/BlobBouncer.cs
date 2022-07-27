96fb293239956551a24ca7cea279c773de21e809
August Alexandersson <augustalexandersson@gmail.com>
2022-07-28 00:16:40 +0200

    ...

--- Assets/BlobBouncer.cs
+++ Assets/BlobBouncer.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class BlobBouncer : MonoBehaviour
@@ -6,32 +5,36 @@ public class BlobBouncer : MonoBehaviour
     // Public
     public float minHeight = 0f;
     public float maxHeight = 1f;
-
     public float bounceLength = .75f;
 
-    public float squishFactor = .2f;
-    public float normalSquishHeight = 1f;
-    public float minSquishHeight = .3f;
-
     // Private
+    private const float NormalSquishHeight = 1f;
     private int _direction = 1;
     private float _startAt;
     private Pickupable _pickupable;
     private EatlingBabyGrowth _eatlingBabyGrowth;
     private bool _bouncing;
+    private float _startedSquish;
+    private float _squishState;
+    private int _squishIndex;
+    private readonly float[] _squishSequence = new[] {.2f, 1.5f, .5f, 1f, .8f, 1f};
+    private readonly float[] _squishSequenceLengths = new[] {.2f, .15f, .1f, .05f, .02f, .01f};
+    private Vector3 _originalScale;
+    private Rigidbody _rigidbody;
 
     // Private
 
     private void Awake()
     {
+        _rigidbody = GetComponent<Rigidbody>();
         _pickupable = GetComponent<Pickupable>();
         _eatlingBabyGrowth = GetComponent<EatlingBabyGrowth>();
+
+        _originalScale = transform.localScale;
     }
 
     void Update()
     {
-        return;
-        
         if (_pickupable.IsPickedUp() || _eatlingBabyGrowth.IsPlanted())
         {
             if (_bouncing)
@@ -55,43 +58,26 @@ public class BlobBouncer : MonoBehaviour
         );
         transform.position = newPosition;
 
-        var currentScale = transform.localScale;
-        var newScale = new Vector3(
-            currentScale.x,
-            HeightScale(),
-            currentScale.z
-        );
-        transform.localScale = newScale;
+        HandleSquishing();
     }
 
-    private float HeightScale()
+    private void HandleSquishing()
     {
-        var progress = (transform.position.y - minHeight) / maxHeight;
+        if (_direction < 0)
+        {
+            var progress = (Time.time - _startAt) / BounceLength();
+            Debug.Log(progress);
+            if (progress > .25f && (_squishIndex < 0))
+            {
+                StartSquish();
+            }
+        }
+        else
+        {
+            _squishIndex = -1;
+        }
 
-        return Mathf.Max(EaseOutBounce(progress) * normalSquishHeight, minSquishHeight);
-        // if (_direction < 0)
-        // {
-        //     var squishProgressNonClamped = (progress - (progress - squishFactor));
-        //     if (squishProgressNonClamped >= 0f)
-        //     {
-        //         var progressOfSquishUp = Mathf.Clamp(squishProgressNonClamped / squishFactor, 0f, 1f);
-        //
-        //         return (1 - SquishEase(1 - progressOfSquishUp)) * normalSquishHeight;
-        //     }
-        //     else
-        //     {
-        //         return minSquishHeight;
-        //     }
-        // }
-        // else
-        // {
-        //     var progressOfSquishUp = Mathf.Clamp(progress / squishFactor, 0f, 1f);
-        // }
-    }
-
-    private float SquishEase(float x)
-    {
-        return 1f;
+        UpdateSquish();
     }
 
     private float BounceProgress()
@@ -103,6 +89,45 @@ public class BlobBouncer : MonoBehaviour
     private float EaseInQuart(float x)
     {
         return x * x * x * x;
+    }
+
+    private void StartSquish()
+    {
+        _startedSquish = Time.time;
+        _squishState = 1f;
+        _squishIndex = 0;
+    }
+
+    private void UpdateSquish()
+    {
+        if (_squishIndex >= _squishSequence.Length || _squishIndex < 0)
+        {
+            _squishState = 1f;
+        }
+        else
+        {
+            var squishTarget = _squishSequence[_squishIndex];
+            var squishDuration = _squishSequenceLengths[_squishIndex];
+
+            var time = Time.time - _startedSquish;
+            if (time > squishDuration)
+            {
+                _squishIndex += 1;
+                _squishState = squishTarget;
+                _startedSquish = Time.time;
+            }
+            else
+            {
+                var previousTarget = _squishIndex == 0 ? NormalSquishHeight : _squishSequence[_squishIndex - 1];
+                _squishState = Mathf.Lerp(previousTarget, squishTarget, time / squishDuration);
+            }
+        }
+
+        transform.localScale = new Vector3(
+            _originalScale.x + _originalScale.x * Mathf.Clamp(1f - _squishState, 0f, 1f),
+            NormalSquishHeight * _squishState,
+            _originalScale.z + _originalScale.z * Mathf.Clamp(1f - _squishState, 0f, 1f)
+        );
     }
 
     private float EaseOutBounce(float x)
@@ -137,5 +162,17 @@ public class BlobBouncer : MonoBehaviour
     {
         _direction *= -1;
         _startAt = Time.time;
+
+        if (_direction > 0)
+        {
+            MoveInRandomDirection();
+        }
+    }
+
+    private void MoveInRandomDirection()
+    {
+        var direction2d = Random.insideUnitCircle;
+        var direction = new Vector3(direction2d.x, 0f, direction2d.y);
+        _rigidbody.AddForce(direction.normalized * 10f, ForceMode.Impulse);
     }
 }
