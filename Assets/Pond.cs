ca3a33888446cbc0c4781d5c912c80f46b08c898
August Alexandersson <augustalexandersson@gmail.com>
2022-07-29 17:20:37 +0200

    Removed unused code

--- Assets/Pond.cs
+++ Assets/Pond.cs
@@ -2,11 +2,6 @@ using UnityEngine;
 
 public class Pond : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        
-    }
-
     public void TryApplyItem(GameObject item)
     {
         var waterCan = item.GetComponent<WateringCan>();
