e7ff63f76e7c4825e19d497b4aa3e116a16106a0
August Alexandersson <augustalexandersson@gmail.com>
2022-07-31 19:11:04 +0200

    Wareworm WIP

--- Assets/WarewormMovement.cs
+++ Assets/WarewormMovement.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 
 public class WarewormMovement : MonoBehaviour
 {
+    public GameObject billboard;
+
     private Transform _target;
     private Rigidbody _rigidbody;
+    private Vector3 _originalScale;
+
 
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _originalScale = billboard.transform.localScale;
     }
 
     void Update()
@@ -19,15 +24,25 @@ public class WarewormMovement : MonoBehaviour
         else
         {
             var direction = (_target.position - transform.position).normalized;
-            _rigidbody.AddForce(direction * 10f * Time.deltaTime, ForceMode.Acceleration);
+            Debug.Log("MOVE! " + _target.gameObject.name);
+
+            var mode = Mathf.RoundToInt(Time.time * 4f) % 2 == 0;
+            _rigidbody.AddForce(direction * (mode ? 100f : 600f) * Time.deltaTime, ForceMode.Force);
         }
+
+        var currentScale = billboard.transform.localScale;
+        var inverted = (Mathf.Max(_rigidbody.velocity.x, _rigidbody.velocity.z) >= 0 ? -1f : 1f);
+        Debug.Log("INVERTD: " + inverted + ", VEL: " + _rigidbody.velocity);
+        billboard.transform.localScale = new Vector3(
+            _originalScale.x * inverted,
+            currentScale.y, currentScale.z);
     }
 
     private void FindTarget()
     {
         var farmers = FindObjectsOfType<FarmerMovement>();
         if (farmers.Length == 0) return;
-        
+
         var farmer = farmers.OrderBy(_ => Random.value).First();
         _target = farmer.transform;
     }
